b6be64c3f6ac9051c7cca90b08e347a8f6a04c0c
tsinghtech <160695143+tsinghtech-research@users.noreply.github.com>
2024-08-08 16:02:19 +0800

    update to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='greenthread',
-    version='0.1.0',
+    version='0.1.1',
     author='tsinghtech',
     author_email="vision@qingtong.cn",
     description='A simple green thread library compatible eventlet and gevent',
